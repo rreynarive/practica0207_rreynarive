2baf5d04e1707d12941fee2b518b47aa3719a5cd
Rosa Reyna <rreynarive1@educacion.navarra.es>
2022-11-25 01:43:30 +0100

    BJVGDI

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -3,14 +3,18 @@
 
 def binario(num):
     binario = " "
-    if num // 2 != 0:
-        binario = str(num % 2)
-        binario = list(binario)
-        binario.reverse()
-    num = num // 2
-    print(binario)
-    return
+    while num // 2 != 0:
+        binario = str(num % 2) + binario
+        num = num // 2
+    return str(num) + binario
 
 num = int(input("Introduce un numero decimal para combertir en binario:\n"))
 print(binario(num))
+
+def decimal(num):
+    for a, b in range(len(binario(num[::-1]))):
+        numero_decimal +=  * 2 ** 
+
+
+
 
